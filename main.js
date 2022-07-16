3c4f3d52f1307fff275a8c5ed1b2676d5fc5760f
Inmaculada Marqués Llaberia <inmaculada.marques@students.salle.url.edu>
2022-07-16 12:56:11 +0200

    module export

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function menuLinks() {
+    document.addEventListener('DOMContentLoaded', (event) => {
+        let menuLinks = document.getElementsByClassName("nav-link");
+        [...menuLinks].forEach(element => indexOf(element));
+    })
+}
+
+export { menuLinks };
+
+
+
+
+
+
