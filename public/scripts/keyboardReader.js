ee2be39c0cc6725d9dad894bc209c79bf0ba8f6f
burningenzo <hombrex_ltf@hotmail.com>
2022-07-16 14:46:47 +0200

    New ver

--- public/scripts/keyboardReader.js
+++ public/scripts/keyboardReader.js
@@ -1,4 +1,21 @@
-function keyboardReader () {
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js";
+
+class KeynavController extends Controller {
+  connect() {
+    console.log("KeynavController connected");
+  }
+
+  follow(event) {
+    //let code = event.code;
+    // ... convert to number
+    //let number = parseInt(code.slice(code.length - 1));
+    let number = event.key;
+    let link = this.element.querySelectorAll(".nav-link")[number];
+    link && link.click();
+  }
+}
+
+function keyboardReader() {
   const navLinks = document.getElementsByClassName("nav-link");
 
   Array.from(navLinks).forEach((element, index) => {
@@ -8,6 +25,6 @@ function keyboardReader () {
       }
     });
   });
- };
+}
 
-export default keyboardReader;
+export { keyboardReader, KeynavController };
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,11 @@
-import dateAlert from '../scripts/dateAlert.js'
-import divColorizer from '../scripts/divColorizer.js'
-import keyboardReader from '../scripts/keyboardReader.js'
+import dateAlert from "../scripts/dateAlert.js";
+import divColorizer from "../scripts/divColorizer.js";
+import { keyboardReader, KeynavController } from "../scripts/keyboardReader.js";
+import { Application } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
 
 dateAlert();
 divColorizer();
-keyboardReader();
+// keyboardReader();
+
+const app = Application.start()
+app.register("kn", KeynavController)
