8a8183d787c17b6bbc934fd98062a2c6f3a8c7b7
Marisa Posada <marisa.posada@gmail.com>
2022-07-29 18:21:55 +0200

    fetch

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 import { AlertFechaHoy } from "./modules/AlertFechaHoy.js"
-// import {EvidenciaDivs} from "./modules/EvidenciaDivs.js"
-// import {EventosTeclado} from "./modules/EventosTeclado.js"
+import {EvidenciaDivs} from "./modules/EvidenciaDivs.js"
+import {EventosTeclado} from "./modules/EventosTeclado.js"
 AlertFechaHoy();
 
 
@@ -13,9 +13,8 @@ const app = Application.start()
 
 app.register("kn", KeynavController)
 
-
 async function getMyData() {
     let response = await fetch("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}");
     let data = await response.json();
     console.log(data)
-   }
+}
