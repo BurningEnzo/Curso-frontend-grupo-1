cac8b9a3f29b1c6c1ed870f66e387910ef006950
Inmaculada Marqués Llaberia <inmaculada.marques@students.salle.url.edu>
2022-07-30 13:32:02 +0200

    trying inserta data div

--- public/scripts/weathermap.js
+++ public/scripts/weathermap.js
@@ -1,18 +1,21 @@
 
 async function getMyData() {
-  let response = await fetch(    
-    "https://api.openweathermap.org/data/2.5/weather?lat=41.3879&lon=2.16992&appid=98c5a609f513ca79bdbd1c97df82d17e"    
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=41.3879&lon=2.16992&units=metric&lang=ca&appid=98c5a609f513ca79bdbd1c97df82d17e"
   );
-  let data = await response.json();
-  console.log('data ',data);
+  const data = await response.json();  
+  console.log('data ', data);
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {  
+getMyData();
+
+document.addEventListener("DOMContentLoaded", function (event) {
 
   const divWeather = document.getElementById("apiWeather");
-  console.log('divWeather ',divWeather);
 
-  getMyData();
+  console.log('divWeather ', divWeather);
 
- 
+  console.log('weather.data2');
+  divWeather.innerHTML = weather.data;
 })
+
