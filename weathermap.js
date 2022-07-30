7708c01086512d7115b4fcfb10c66b084eefbcb7
Inmaculada Marqués Llaberia <inmaculada.marques@students.salle.url.edu>
2022-07-30 10:03:22 +0200

    acces data openweathermap with node

--- weathermap.js
+++ weathermap.js
@@ -1,11 +1,11 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 async function getMyData() {
-  let response = await fetch(
-    "https://api.openweathermap.org/data/3.0/onecall?lat={41.3879}&lon={2.16992}&appid={98c5a609f513ca79bdbd1c97df82d17e}"
+  let response = await fetch(    
+    "https://api.openweathermap.org/data/2.5/weather?lat=41.3879&lon=2.16992&appid=98c5a609f513ca79bdbd1c97df82d17e"    
   );
   let data = await response.json();
-  console.log(data);
+  console.log('data ',data);
 }
 
-modules.exports={getMyData};
+getMyData();
